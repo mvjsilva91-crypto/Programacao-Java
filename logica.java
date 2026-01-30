095ba2a23ed51c631dbfea72d5ab2136ab947b24
mvjsilva91-crypto <mvjsilva91@gmail.com>
2026-01-29 21:52:34 -0300

    Arquivo lógica

---
+++ logica.java
@@ -0,0 +1,10 @@
+public class logica {
+    public static void main(String[] args) {
+        int x, y, z;
+        x = 4; y = 7; z = 12; boolean r;
+        r = (x < y && y < z);
+
+        System.out.println(r);
+    }
+}
+
