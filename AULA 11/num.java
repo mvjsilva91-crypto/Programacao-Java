aed7a5d82867070b5add0b4fe39ac6470f4adc7e
mvjsilva91-crypto <mvjsilva91@gmail.com>
2026-02-03 19:47:14 -0300

    atual

--- AULA 11/num.java
+++
@@ -1,9 +0,0 @@
-public class num {
-    public static void main(String[] args){
-        int numero = 10;
-        System.out.println(numero);
-        numero++;
-        System.out.println(numero);
-    }
-    
-}
