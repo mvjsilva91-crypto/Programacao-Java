5d276d70ea78e6b1297147660949d6120690fd5f
mvjsilva91-crypto <mvjsilva91@gmail.com>
2026-02-04 20:13:08 -0300

    A

--- AULA 14/Acionador.java
+++ AULA 14/Acionador.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Acionar {
+public class Acionador {
     public static void main(String[] args) {
         Scanner acn = new Scanner(System.in);
         int btncor;
