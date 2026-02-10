d1da9a7fe8846b539d74a270f41396ee8d51593f
mvjsilva91-crypto <mvjsilva91@gmail.com>
2026-02-10 19:21:28 -0300

    a

---
+++ AULA 17/Ex1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char resp;
+
+        do {
+            System.out.print("Digite a temperatura em Celsius: ");
+            double c = sc.nextDouble();
+
+            double f = (c * 9 / 5) + 32.0;
+            System.out.printf("Equivalente em Fahrenheit: %.1f%n", f);
+
+            System.out.print("Deseja repetir [s/n]? ");
+            resp = sc.next().charAt(0);
+
+        } while (resp != 'n');
+
+        sc.close();
+    }
+}
+
