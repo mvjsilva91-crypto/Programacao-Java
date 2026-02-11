2c57e2463252d8852011d4fbb3833a00b25e0973
mvjsilva91-crypto <mvjsilva91@gmail.com>
2026-02-11 20:18:17 -0300

    Atual

--- AULA 18/matriz.java
+++ AULA 18/matriz.java
@@ -1,21 +1,40 @@
-import java.util.Scanner
+import java.util.Scanner;
 
-public class matriz {
+public class Matriz {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int m , n;
-        System.out.print("Qual a quantidade de Linhas de matriz");
+
+        int m, n;
+
+        System.out.print("Qual a quantidade de Linhas da matriz: ");
         m = sc.nextInt();
-        System.out.print("Qual a quantidade de Colunas de matriz");
+
+        System.out.print("Qual a quantidade de Colunas da matriz: ");
         n = sc.nextInt();
 
-        int[][] matriz = new int [n][m];
+        int[][] matriz = new int[m][n];
+
+       
+
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                System.out.printf("Elemento [%d,%d]: ",i,j);
+                System.out.printf("Elemento [%d,%d]: ", i, j);
                 matriz[i][j] = sc.nextInt();
             }
         }
+
+       
+        System.out.println("Valores Negativos:");
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(matriz[i][j] < 0) {
+                    System.out.println(matriz[i][j]);
+                }
+            } 
+        }
+
+        sc.close();
     }
-    
 }
+
+           
